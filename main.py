7605f5c03e80aaa83b448084dd814547c9c1faee
BIRCK <marcobirck@outlook.com>
2016-04-21 20:31:51 -0300

    vem que vem

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 
 im = cv2.imread('img/test.jpg',1)
 img = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-cv2.namedWindow("Display window",cv2.WINDOW_NORMAL)
-cv2.imshow("Display window",img)
+cv2.namedWindow("Colorfull image",cv2.WINDOW_NORMAL)
+cv2.namedWindow("Gray image",cv2.WINDOW_NORMAL)
+cv2.imshow("Colorfull image",im)
+cv2.imshow("Gray image",img)
 cv2.waitKey(0)
