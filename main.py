26c8968ed2f729412e4367e9dac9a5d4e61ef41a
BIRCK <marcobirck@outlook.com>
2016-04-21 20:16:31 -0300

    gray the image

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 
-im = cv2.imread('img\test.jpg')
+im = cv2.imread('img/test.jpg',1)
 img = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-cv2.namedWindow("Display window",WINDOW_AUTOSIZE)
-cv.imshow("input",img)
+cv2.namedWindow("Display window",cv2.WINDOW_NORMAL)
+cv2.imshow("Display window",img)
+cv2.waitKey(0)
